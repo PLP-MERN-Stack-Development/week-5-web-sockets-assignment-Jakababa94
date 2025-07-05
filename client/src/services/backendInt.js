d3e2f77d0cc034d52f04327dcfaf429b8772072a
Jakababa94 <jakababa002@gmail.com>
2025-07-05 16:08:17 +0300

    Backend-Frontend connection

---
+++ client/src/services/backendInt.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { io } from "socket.io-client"
+
+const BackendBaseUrl = "http://localhost:500";
+const APIBaseUrl = "http://localhost:5000/api";
+
+const API = axios.create({
+    baseURL: APIBaseUrl
+});
+
+// Connect Backend to Frontend
+export const registerUser = ( username )=>
+    API.post("/auth/register", { username });
+
+export const getRooms = ()=> API.get("/rooms");
+
+export const createRoom = (name)=> API.post("/rooms", { name });
+
+export const getMessages = (roomId)= API.get(`/message/$(roomId)`);
+
+export const socket = io(BackendBaseUrl, { autoConnect: false})
